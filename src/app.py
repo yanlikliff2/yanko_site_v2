9dd78db7f983389bb9c02f5cde0be23bbb3430c8
yanlikliff2 <167241536+yanlikliff2@users.noreply.github.com>
2024-05-12 14:25:33 +0300

    readme и port

--- src/app.py
+++ src/app.py
@@ -85,4 +85,4 @@ def promsrab():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0") #debug=True, 
+    app.run(host="0.0.0.0", port=1100) #debug=True, 
