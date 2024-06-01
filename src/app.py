33317f3be0d7eb2b41f13bd4125f9c4d003bc518
yanlikliff2 <167241536+yanlikliff2@users.noreply.github.com>
2024-06-01 17:07:00 +0300

    изменение анимации

--- src/app.py
+++ src/app.py
@@ -91,6 +91,14 @@ def backtolog():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(401)
+def unauthorized(e):
+    return render_template('401.html'), 401
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
